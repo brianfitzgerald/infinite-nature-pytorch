410fc0abb27044412d4aae32aa50a2e92c9a7aee
Brian Fitzgerald <brian.fitzgerald@equipmentshare.com>
2022-09-21 12:37:00 -0500

    copy over loss fns

--- loss.py
+++ loss.py
@@ -129,3 +129,115 @@ def compute_discriminator_loss(real_logit, fake_logit):
   disc_loss = real_loss + fake_loss
 
   return disc_loss, real_accuracy, fake_accuracy
+
+def compute_adversarial_loss(fake_logit):
+  """Computes the adversarial hinge loss to apply to the generator.
+
+  Args:
+    fake_logit: list of tensors which correspond to discriminator logits
+      on generated images
+
+  Returns:
+    A scalar of the loss.
+  """
+  fake_loss = 0
+  for fake_l in fake_logit:
+    fake_loss += -torch.mean(fake_l)
+
+  # average of all.
+  fake_loss = fake_loss / float(len(fake_logit))
+
+  return fake_loss
+
+
+def compute_feature_matching(real_feats, fake_feats):
+  """Computes a feature matching loss between real and fake feature pyramids.
+
+  Args:
+    real_feats: A list of feature activations of a discriminator on real images
+    fake_feats: A list of feature activations on fake images
+
+  Returns:
+    A scalar of the loss.
+  """
+  losses = []
+  # Loop for scale
+  for real_feat, fake_feat in zip(real_feats, fake_feats):
+    losses.append(torch.mean(torch.abs(real_feat - fake_feat)))
+
+  return torch.mean(losses)
+
+def compute_perceptual_loss(generated, real):
+  """Compute the perceptual loss between a generated and real sample.
+
+  The input to this are RGB images ranging from [0, 255].
+
+  build_vgg19's reference library can be found here:
+  https://github.com/CQFIO/PhotographicImageSynthesis/blob/master/demo_1024p.py
+
+  Args:
+    generated: [B, H, W, 3] Generated image that we want to be perceptually
+      close to real.
+    real: [B, H, W, 3] Ground truth image that we want to target.
+
+  Returns:
+    A tf scalar corresponding to the perceptual loss.
+  """
+  # Input is [0, 255.], not necessarily clipped though
+  def build_vgg19(*args):
+    raise NotImplementedError
+  vgg_real_fake = build_vgg19(
+      torch.concat([real, generated], axis=0),
+      "imagenet-vgg-verydeep-19.mat")
+
+  def compute_l1_loss(key):
+    real, fake = torch.split(vgg_real_fake[key], 2, axis=0)
+    return torch.reduce_mean(torch.abs(real - fake))
+
+  p0 = torch.zeros([])
+  p1 = compute_l1_loss("conv1_2") / 2.6
+  p2 = compute_l1_loss("conv2_2") / 4.8
+  p3 = compute_l1_loss("conv3_2") / 3.7
+  p4 = compute_l1_loss("conv4_2") / 5.6
+  p5 = compute_l1_loss("conv5_2") * 10 / 1.5
+  return p0 + p1 + p2 + p3 + p4 + p5
+
+def compute_perceptual_loss(generated, real):
+  """Compute the perceptual loss between a generated and real sample.
+
+  The input to this are RGB images ranging from [0, 255].
+
+  build_vgg19's reference library can be found here:
+  https://github.com/CQFIO/PhotographicImageSynthesis/blob/master/demo_1024p.py
+
+  Args:
+    generated: [B, H, W, 3] Generated image that we want to be perceptually
+      close to real.
+    real: [B, H, W, 3] Ground truth image that we want to target.
+
+  Returns:
+    A tf scalar corresponding to the perceptual loss.
+  """
+  # Input is [0, 255.], not necessarily clipped though
+  def build_vgg19(*args):
+    raise NotImplementedError
+  vgg_real_fake = build_vgg19(
+      torch.concat([real, generated], axis=0),
+      "imagenet-vgg-verydeep-19.mat")
+
+  def compute_l1_loss(key):
+    real, fake = torch.split(vgg_real_fake[key], 2, axis=0)
+    return torch.mean(torch.abs(real - fake))
+
+  p0 = torch.zeros([])
+  p1 = compute_l1_loss("conv1_2") / 2.6
+  p2 = compute_l1_loss("conv2_2") / 4.8
+  p3 = compute_l1_loss("conv3_2") / 3.7
+  p4 = compute_l1_loss("conv4_2") / 5.6
+  p5 = compute_l1_loss("conv5_2") * 10 / 1.5
+  return p0 + p1 + p2 + p3 + p4 + p5
+
+def compute_kld_loss(mu, logvar):
+  loss = -0.5 * torch.sum(1 + logvar - torch.square(mu) - torch.exp(logvar))
+  return loss
+
